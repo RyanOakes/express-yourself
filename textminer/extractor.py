303c10e4c184cf7cd3424e999b682c9b88d102c1
oakes <oakesonline@gmail.com>
2016-04-19 21:39:21 -0400

    Validator progress updates

---
+++ textminer/extractor.py
@@ -0,0 +1,6 @@
+# import re
+#
+#
+# def phone_numbers(text):
+#     # TODO: regex shit
+#     return ["(454) 999-1212", "(919) 123-4569"]
---
+++ textminer/separator.py
@@ -0,0 +1,22 @@
+import re
+
+
+def words(value):
+    return []
+
+def phone_numbers(value):
+    return value
+
+
+def money(value):
+    """We are just concerned with dollars here for now but might take other
+    currencies later."""
+    return value
+
+
+def zip(value):
+    return value
+
+
+def date(value):
+    return value
